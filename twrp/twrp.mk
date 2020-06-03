9209232673c95e713a8c96a633031de9c3e62602
Ardjlon <carlosardany@gmail.com>
2020-06-03 21:58:55 +0000

    evert: Update TWRP by seba_3567

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -1,5 +1,5 @@
 TWRP_PATH := device/motorola/evert/twrp
 
-TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(TWRP_PATH)/ramdisk-twrp-seba.cpio
+TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(TWRP_PATH)/ramdisk-twrp-r21_by-seba_3567.cpio
 #TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-twrp.img
 #TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-twrp.zip
