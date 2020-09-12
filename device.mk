6c31dff1cde151b097cdd29225027b02882d3954
trpsl <trpsl755@gmail.com>
2020-09-12 18:15:26 +0200

    evert: Build MotoActions

--- device.mk
+++ device.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sdm660
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
