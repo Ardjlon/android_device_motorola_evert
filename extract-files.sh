c570143212d0886243576a64357dfb3334ca1de7
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-10-24 10:08:46 -0400

    evert: Migrate to function blob_fixup() for patching blobs
    Change-Id: I45e3454ecd8f5af17b00e6ad2b3001fc82190e88

--- extract-files.sh
+++ extract-files.sh
@@ -15,6 +15,22 @@
 # limitations under the License.
 #
 
+function blob_fixup() {
+    case "${1}" in
+
+        # Load ZAF configs from vendor
+        vendor/lib/libzaf_core.so)
+            sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "${2}"
+            ;;
+
+        # Add uhid group for fingerprint service
+        vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+            sed -i "s/input/uhid input/" "${2}"
+            ;;
+
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -22,6 +38,7 @@ if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
 fi
 
 set -e
+
 AOSP_ROOT="$MY_DIR"/../../..
 
 # Required!
